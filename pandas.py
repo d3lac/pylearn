d2f89321af19582f7ed80e692fd47117f4648bb4
d3lac <75080720+d3lac@users.noreply.github.com>
2022-02-01 19:56:11 +0530

    Delete pandas.py

--- pandas.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-list = ('Carrot', 'Beans', 'Cauliflower')
-# create data frame
-titled_columns = {"name": list,
-                 "height": [1.67, 1.9, 0.25],
-                 "weight": [54, 100, 1]}
-data = pd.DataFrame(titled_columns)
-print(data)
